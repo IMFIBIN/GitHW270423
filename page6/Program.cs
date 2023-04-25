459f5115467ec9c6d32b44f9c738edc1cd9fcd57
Danila Oreshkin <korobkamy50@yandex.ru>
2023-04-25 11:35:10 +0300

    P6

---
+++ page6/Program.cs
@@ -0,0 +1,11 @@
+﻿//  Программа на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+Console.WriteLine ("Введите третье число: ");
+int count = Convert.ToInt32(Console.ReadLine());
+if (count%2 == 0)
+{
+    Console.WriteLine("Yes");
+}
+else
+{
+    Console.WriteLine("No");
+}
