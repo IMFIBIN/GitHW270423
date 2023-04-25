9d08516039880fc461e51dad5a13fb95b020051d
Danila Oreshkin <korobkamy50@yandex.ru>
2023-04-25 12:10:47 +0300

    P8

--- page8/Program.cs
+++ page8/Program.cs
@@ -1,1 +1,20 @@
 ﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+Console.WriteLine ("Введите число: ");
+int count = Convert.ToInt32(Console.ReadLine());
+int i = 1;
+bool shouldRun = true;
+Console.WriteLine("Чётные числа от 1 до " + count);
+while (i <= count)
+{
+    if (i % 2 != 1)
+    {
+        Console.Write(i + ", ");
+        shouldRun = false;
+    }
+    i++;
+    }
+
+if (shouldRun)
+{
+    Console.WriteLine("Нет чётных чисел!");
+}
