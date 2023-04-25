716e18ef6ded77adb60058334309d05f52d7ee67
Danila Oreshkin <korobkamy50@yandex.ru>
2023-04-25 11:38:12 +0300

    Fix bug

--- page6/Program.cs
+++ page6/Program.cs
@@ -1,5 +1,5 @@
 ﻿//  Программа на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
-Console.WriteLine ("Введите третье число: ");
+Console.WriteLine ("Введите число: ");
 int count = Convert.ToInt32(Console.ReadLine());
 if (count%2 == 0)
 {
